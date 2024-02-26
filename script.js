749da2d21d99e9932573c53f42c5441691ef713d
Diego <diazdiad@oregonstate.edu>
2024-02-25 19:38:15 -0800

    trying to make dark mode better

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const messageInput = document.getElementById('message-input');
 const messageContainer = document.getElementById('message-container');
 const darkModeToggle = document.querySelector('.dark-mode-toggle');
 const userNameElement = document.getElementById('user-name');
+const storageKey = 'theme-preference'
 
 let userName = prompt('What is your name?');
 if (!userName) userName = "Anonymous";
@@ -109,16 +110,56 @@ socket.on('stop typing', (data) => {
   typingIndicator.style.display = 'none';
 });
 
-function toggleDarkMode() {
-  document.body.classList.toggle('dark-mode');
-  messageForm.classList.toggle('dark-mode');
-  messageInput.classList.toggle('dark-mode-input');
-  messageContainer.classList.toggle('dark-mode');
-  messageContainer.scrollTop = messageContainer.scrollHeight;
-  const menuBar = document.getElementById('menu-bar');
-  menuBar.classList.toggle('dark-mode');
+const onClick = () => {
+  // flip current value
+  theme.value = theme.value === 'light'
+    ? 'dark'
+    : 'light'
+
+  setPreference()
 }
 
+const getColorPreference = () => {
+  if (localStorage.getItem(storageKey))
+    return localStorage.getItem(storageKey)
+  else
+    return window.matchMedia('(prefers-color-scheme: dark)').matches
+      ? 'dark'
+      : 'light'
+}
+
+const setPreference = () => {
+  localStorage.setItem(storageKey, theme.value)
+  reflectPreference()
+}
+
+const reflectPreference = () => {
+  document.firstElementChild
+    .setAttribute('data-theme', theme.value)
+
+  document
+    .querySelector('#theme-toggle')
+    ?.setAttribute('aria-label', theme.value)
+}
+
+const theme = {
+  value: getColorPreference(),
+}
+
+// set early so no page flashes / CSS is made aware
+reflectPreference()
+
+window.onload = () => {
+  // set on load so screen readers can see latest value on the button
+  reflectPreference()
+
+  // now this script can find and listen for clicks on the control
+  document
+    .querySelector('#theme-toggle')
+    .addEventListener('click', onClick)
+}
+
+// sync with system changes
 window
   .matchMedia('(prefers-color-scheme: dark)')
   .addEventListener('change', ({matches:isDark}) => {
